83ef3b0c59c994c55914c64f144147aad8ad933a
abbas-akbarzade <aakbarzade22@gmail.com>
2025-12-31 02:47:47 -0800

    Encoder Complete

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,10 +1,37 @@
 #define CLK_PIN 2  //  Clock+ Pin - Yellow
 #define DATA_PIN 3  //  Data+ Pin - White
-#define S1
+#define SSI_BITS 18  // 18bit resolution
 
 
+#define RESOLUTION 262144
 
+void setup() {
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+  pinMode(CLK_PIN, OUTPUT);
+  pinMode(DATA_PIN, INPUT);
+  digitalWrite(CLK_PIN, HIGH);  //  Clock Initialization
+  Serial.begin(19200);
+}
 
+void loop() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    int Command = command.toInt();
+    if (Command == 1) {
+      Serial.println("Encoder is ready!");
+      digitalWrite(4, HIGH);
+      delay(300);
+      digitalWrite(4, LOW);
+    }
+    else if (Command == 0) {
+      unsigned long grayCode = readSSI();  //  readSSI Function Call
+      unsigned long position = grayToBinary(grayCode);  // grayToBinary Function Call
+      float angle = (float(position) / RESOLUTION) * 360.0; // Binary to Angle Scale
+    //Serial.print("Gray Code: ");
+    //Serial.print(grayCode, BIN);
+    //Serial.print(" | Position: ");
+    //Serial.print(position);
     //Serial.print(" | Angle: ");
       Serial.println(angle, 3);
     //delay();
